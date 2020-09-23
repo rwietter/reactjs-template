8d2834f3f4a1b205ae505fb6e6805aa34c15df6b
Maurício Witter <46854467+rwietter@users.noreply.github.com>
2020-09-22 22:07:01 -0300

    feat: add prettier ignore and rename error boundary path

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ErrorBoundary } from './components/ErrorBoundary/ErrorNoundary.jsx';
+import { ErrorBoundary } from './domain/Error/ErrorNoundary.jsx';
 import Routes from './routes.jsx';
 import * as serviceWorker from './serviceWorker.js';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ErrorBoundary render={() => <h1>Oooh Nooo! An error occurred to the open app.</h1>}>
+		<ErrorBoundary
+			render={() => <h1>Oooh Nooo! An error occurred to the open app.</h1>}
+		>
 			<Routes />
 		</ErrorBoundary>
 	</React.StrictMode>,
